5e80d2a64ab712beea4c074630fa8f075d74f394
Ave-Mari <avemari98@gmail.com>
2023-01-08 22:18:43 +0300

    add cart item layout

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import { CartItemType } from '../App';
 //styles
 import { Wrapper } from './CartItem.styles'
+import { amber } from "@mui/material/colors";
 
 type Props = {
     item: CartItemType;
@@ -12,10 +13,33 @@ type Props = {
 }
 
 
-const CartItem : React.FC<Props> = () => {
-    return (
-        <></>
-    )
-}
+const CartItem : React.FC<Props> = ( { item, addToCart, removeFromCart } ) => (
+    <Wrapper> 
+        <div> 
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className="buttons">
+                <Button size="small" 
+                disableElevation 
+                variant="contained" 
+                onClick={() => removeFromCart(item.id)}
+                >
+                -
+                </Button>
+                <p>{item.amount}</p>
+                <Button size="small" 
+                disableElevation 
+                variant="contained" 
+                onClick={() => addToCart(item)}
+                >
+                +
+                </Button>
+            </div>
+        </div>
+    </Wrapper>
+)
 
 export default CartItem;
